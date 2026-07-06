9c618deed31fb24b2127e33154968d00116522a6
Deep Lakshmi <deeplaxmidubey7@gmail.com>
2026-07-06 19:07:16 +0530

    Updated support page and other components

--- src/landing_page/about/AboutPage.js
+++ src/landing_page/about/AboutPage.js
@@ -3,17 +3,11 @@ import React from 'react';
 import Hero from './Hero';
 import Team from './Team';
 
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-
 function AboutPage() {
   return ( 
     <>
-        <h1>About Page</h1>
-        <Navbar />
         <Hero />
         <Team />
-        <Footer />
     </>
    );
 }
