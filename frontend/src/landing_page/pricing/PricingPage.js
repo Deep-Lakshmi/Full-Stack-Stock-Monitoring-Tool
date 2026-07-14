915ea6091e48ae1cceae7ea6115b62b556d798d4
Deep Lakshmi <deeplaxmidubey7@gmail.com>
2026-07-14 13:57:25 +0530

    Completed frontend updates

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Hero from "./Hero";
+import OpenAccount from "../OpenAccount";
+import Brojkerage from "./Brojkerage";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <OpenAccount />
+      <Brojkerage />
+    </>
+  );
+}
+
+export default PricingPage;
